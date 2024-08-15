c4c515137f36e21bdd850347ca552e1031c2aa15
aminebenmahdi <amine.benmahdi@ecole2600.com>
2024-08-16 01:11:34 +0200

    diffie_hellman.py

--- src/cryptoy/diffie_hellman.py
+++ src/cryptoy/diffie_hellman.py
@@ -1,22 +1,14 @@
 import random
 import sys
 
-from cryptoy.utils import (
-    pow_mod,
-)
+from cryptoy.utils import pow_mod
 
-sys.setrecursionlimit(5000)  # Required for pow_mod for large exponents
-
+sys.setrecursionlimit(5000)  # Necessary for large exponentiation in pow_mod
 
 def keygen(prime_number: int, generator: int) -> dict[str, int]:
-    # Implementez la generation de clef de diffie hellman
-    # 1. Tire aléatoirement un nombre secret private_key entre 2 et prime_number - 1 inclus avec random.randint(min, max)
-    # 2. Calcule la clef publique public_key = generator ** private_key % prime_number; utiliser la fonction pow_mod
-    # 3. Renvoit le dictionnaitre {"public_key": A, "private_key": a}
-    pass
-
+    pr = random.randint(2, prime_number - 1)
+    pu = pow_mod(generator, pr, prime_number)
+    return {"public_key": pu, "private_key": pr}
 
 def compute_shared_secret_key(public: int, private: int, prime_number: int) -> int:
-    # Implementer le calcul de la clef secrete partagée à partir de la clef publique de l'autre participant et de ma clef privée
-    # Utiliser pow_mod
-    pass
+    return pow_mod(public, private, prime_number)
