3869fbe98906248e22ef19a33e9c8de72c4695d0
aminebenmahdi <amine.benmahdi@ecole2600.com>
2024-08-16 01:19:51 +0200

    affine_cipher.py

--- src/cryptoy/affine_cipher.py
+++ src/cryptoy/affine_cipher.py
@@ -1,10 +1,7 @@
-
-
 from math import gcd
-
 from cryptoy.utils import str_to_unicodes, unicodes_to_str
 
-# TP: Affine Cipher
+# TP: Chiffrement affine
 
 def compute_permutation(a: int, b: int, n: int) -> list[int]:
     permutation = [(a * i + b) % n for i in range(n)]
@@ -18,56 +15,52 @@ def compute_inverse_permutation(a: int, b: int, n: int) -> list[int]:
     return inverse_perm
 
 def encrypt(msg: str, a: int, b: int) -> str:
-    unicode_vals = str_to_unicodes(msg)
-    perm = compute_permutation(a, b, 0x110000)
-    encrypted_vals = [perm[val] for val in unicode_vals]
-    return unicodes_to_str(encrypted_vals)
+    permutation = compute_permutation(a, b, 0x110000)
+    encoded_message = [permutation[x] for x in str_to_unicodes(msg)]
+    return unicodes_to_str(encoded_message)
 
 def encrypt_optimized(msg: str, a: int, b: int) -> str:
-    unicode_vals = str_to_unicodes(msg)
-    encrypted_vals = [(a * val + b) % 0x110000 for val in unicode_vals]
-    return unicodes_to_str(encrypted_vals)
+    encoded_message = [(a * x + b) % 0x110000 for x in str_to_unicodes(msg)]
+    return unicodes_to_str(encoded_message)
 
 def decrypt(msg: str, a: int, b: int) -> str:
-    unicode_vals = str_to_unicodes(msg)
-    inv_perm = compute_inverse_permutation(a, b, 0x110000)
-    decrypted_vals = [inv_perm[val] for val in unicode_vals]
-    return unicodes_to_str(decrypted_vals)
+    inverse_perm = compute_inverse_permutation(a, b, 0x110000)
+    decoded_message = [inverse_perm[x] for x in str_to_unicodes(msg)]
+    return unicodes_to_str(decoded_message)
 
 def decrypt_optimized(msg: str, a_inverse: int, b: int) -> str:
-    unicode_vals = str_to_unicodes(msg)
-    decrypted_vals = [(a_inverse * (val - b)) % 0x110000 for val in unicode_vals]
-    return unicodes_to_str(decrypted_vals)
+    decoded_message = [(a_inverse * (y - b)) % 0x110000 for y in str_to_unicodes(msg)]
+    return unicodes_to_str(decoded_message)
 
 def compute_affine_keys(n: int) -> list[int]:
-    valid_keys = [k for k in range(1, n) if gcd(k, n) == 1]
-    return valid_keys
+    return [a for a in range(1, n) if gcd(a, n) == 1]
 
 def compute_affine_key_inverse(a: int, affine_keys: list[int], n: int) -> int:
     for key in affine_keys:
         if (a * key) % n == 1:
             return key
-    raise RuntimeError(f"No inverse found for {a}")
+    raise RuntimeError(f"{a} has no inverse")
 
 def attack() -> tuple[str, tuple[int, int]]:
-    encrypted_sample = "࠾ੵΚઐ௯ஹઐૡΚૡೢఊஞ௯\u0c5bૡీੵΚ៚Κஞїᣍફ௯ஞૡΚր\u05ecՊՊΚஞૡԿՊեԯՊ؇ԯրՊրր"
+    encrypted_message = "࠾ੵΚઐ௯ஹઐૡΚૡೢఊஞ௯\u0c5bૡీੵΚ៚Κஞїᣍફ௯ஞૡΚր\u05ecՊՊԿஞૡΚՊեԯՊ؇ԯրՊրր"
     for a in compute_affine_keys(0x110000):
-        decrypted_msg = decrypt(encrypted_sample, a, 58)
-        if "bombe" in decrypted_msg:
-            return decrypted_msg, (a, 58)
-    raise RuntimeError("Attack failed")
+        decrypted_message = decrypt(encrypted_message, a, 58)
+        if "bombe" in decrypted_message:
+            return decrypted_message, (a, 58)
+
+    raise RuntimeError("Failed to attack")
 
 def attack_optimized() -> tuple[str, tuple[int, int]]:
-    encrypted_sample = (
+    encrypted_message = (
         "જഏ൮ൈ\u0c51ܲ೩\u0c51൛൛అ౷\u0c51ܲഢൈᘝఫᘝా\u0c51\u0cfc൮ܲఅܲᘝ൮ᘝܲాᘝఫಊಝ"
         "\u0c64\u0c64ൈᘝࠖܲೖఅܲఘഏ೩ఘ\u0c51ܲ\u0c51൛൮ܲఅ\u0cfc\u0cfcඁೖᘝ\u0c51"
     )
-
     affine_keys = compute_affine_keys(0x110000)
     for a in affine_keys:
         a_inverse = compute_affine_key_inverse(a, affine_keys, 0x110000)
         for b in range(1, 10000):
-            decrypted_msg = decrypt_optimized(encrypted_sample, a_inverse, b)
-            if "bombe" in decrypted_msg:
-                return decrypted_msg, (a, b)
-    raise RuntimeError("Attack failed")
+            decrypted_message = decrypt_optimized(encrypted_message, a_inverse, b)
+            if "bombe" in decrypted_message:
+                return decrypted_message, (a, b)
+
+    raise RuntimeError("Failed to attack")
