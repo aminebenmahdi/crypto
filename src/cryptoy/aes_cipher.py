bd116afc005cd1e5111b27669b40c194e8cce36b
aminebenmahdi <amine.benmahdi@ecole2600.com>
2024-08-16 01:02:01 +0200

    aes_cipher.py

--- src/cryptoy/aes_cipher.py
+++ src/cryptoy/aes_cipher.py
@@ -2,12 +2,9 @@ from cryptography.hazmat.primitives.ciphers.aead import (
     AESGCM,
 )
 
-
 def encrypt(msg: bytes, key: bytes, nonce: bytes) -> bytes:
-    # A implémenter en utilisant la class AESGCM
-    pass
+    return AESGCM(key).encrypt(nonce, msg, None)
 
 
 def decrypt(msg: bytes, key: bytes, nonce: bytes) -> bytes:
-    # A implémenter en utilisant la class AESGCM
-    pass
+    return AESGCM(key).decrypt(nonce, msg, None)
